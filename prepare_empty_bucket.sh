2b00f070f984039c4bde47f549b81a74441fc1db
Ubuntu <ubuntu@ip-172-31-5-255.us-west-1.compute.internal>
2019-09-04 20:51:59 +0000

    changes to the script that prepares the emptying of the bucket

--- prepare_empty_bucket.sh
+++ prepare_empty_bucket.sh
@@ -3,11 +3,12 @@
 # this is a script to prepare the token bucket emptying script
 
 # apt may not know about the iperf3 package unless updated first
-sudo apt update --force-yes
+sudo apt-get -y update --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
 # install iperf3
-sudo apt install iperf3 --force-yes
+sudo apt-get -y install iperf3 --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
 # install python3 dependencies for the token measuring tool
+sudo apt-get -y install python3-pip --allow-downgrades --allow-remove-essential --allow-change-held-packages
 pip3 install psutil
 pip3 install numpy

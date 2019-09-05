bf9a413691e80627ba9f0da488e401f1314bb607
Alex Uta <alexuta@204-99-170-84.uguest.utah.edu>
2019-09-05 10:44:03 -0600

    added more logging to the bucket emptying script

--- empty_bucket.py
+++ empty_bucket.py
@@ -7,6 +7,9 @@ import numpy as np
 import signal
 import sys
 
+outfile_bucket = "token_bucket.txt"
+outfile_data = "traffic_data.txt"
+
 def main(SERVER_IP):
     # fork to start an iperf on the
     pid = os.fork()
@@ -36,11 +39,13 @@ def monitor_bw(iperf_pid):
     # store bw values
     bw_values = []
     # monitor crnt bw
+    count = 0
+    f2 = open(outfile_data, "w")
     while True:
         # get bw
         new_value = psutil.net_io_counters().bytes_sent
         # print it
-        print_stat(new_value - old_value)
+        print_stat(f2, new_value - old_value)
         # add bw_value to bw array
         bw_values.append(to_gbit(new_value - old_value))
         # check if we got a drop in performance
@@ -48,14 +53,25 @@ def monitor_bw(iperf_pid):
             # we have identified a bucket
             print("it took %0.3f seconds to empty the bucket" % len(bw_values))
             print("big bw_value = %0.3f, small bw_value = %0.3f" % (bw_values[2], bw_values[len(bw_values) - 2]))
+            f = open(outfile_bucket, "w")
+            f.write("%0.3f,%0.3f,%0.3f\n" % (len(bw_values), bw_values[2], bw_values[len(bw_values) - 2]))
+            f.close()
+            f2.close()
             os.kill(iperf_pid, signal.SIGINT)
             exit(0)
 
         old_value = new_value
         time.sleep(1)
+        count += 1
+        if (count > 3600):
+			f = open(outfile_bucket, "w")
+			f.write("no bucket identified")
+			f.close()
+			f2.close()
+			break
 
-def print_stat(value):
-    print ("crnt bw = %0.3f" % to_gbit(value))
+def print_stat(f, value):
+    f.write("crnt bw = %0.3f" % to_gbit(value))
 
 # you need to pass the IP of the server as the argument
 main(sys.argv[1])
